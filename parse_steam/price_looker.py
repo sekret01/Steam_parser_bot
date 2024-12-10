4e25b273e6e1253e20769d6d6d142e2112b744f0
Pavel <asinskijp188@gmail.com>
2024-12-10 20:35:36 +0300

    add class PriceLooker with basic functionality

---
+++ parse_steam/price_looker.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+from bs4 import BeautifulSoup
+from requests import Response
+
+
+
+class PriceLooker:
+    """Price parsing and discount search class"""
+    def __init__(self):
+        self.price_block: BeautifulSoup = BeautifulSoup()
+
+    def is_discount(self):
+        if self.price_block.find("div", class_="discount_prices"):
+            return True
+        return False
+
+    def get_origin_price(self) -> str:
+        return self.price_block.find("div", class_="game_purchase_price price").text.strip()
+
+    def get_discount_info(self) -> tuple[str, str, str]:
+        origin_price = self.price_block.find("div", class_="discount_original_price").text.strip()
+        discount_price = self.price_block.find("div", class_="discount_final_price").text.strip()
+        discount_info = self.price_block.find("p", class_="game_purchase_discount_countdown").text.strip()
+        return origin_price, discount_price, discount_info
+
+    def get_price_info(self, page: Response) -> tuple[int, str, any]:
+        soup = BeautifulSoup(page.text, "html.parser")
+        self.price_block = soup.find("div", class_="game_area_purchase_game")
+
+        if self.is_discount():
+            discount = 1
+            origin_price, discount_price, discount_info = self.get_discount_info()
+            discount_data = (discount_price, discount_info)
+        else:
+            discount = 0
+            origin_price = self.get_origin_price()
+            discount_data = None
+
+        return discount, origin_price, discount_data
+
+
+
+
+import requests
+# 3097560
+# 246620
+link = "https://store.steampowered.com/app/3097560"
+p = PriceLooker()
+print(p.get_price_info(requests.get(link)))
