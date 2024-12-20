5630712533c2e30e1f1543474a68b73370af3d4b
Pavel <asinskijp188@gmail.com>
2024-12-20 11:43:07 +0300

    A function has been created to add tracked games to the database

--- steam_database/db_controller.py
+++ steam_database/db_controller.py
@@ -6,12 +6,17 @@ class DatabaseController:
         self.db_path = "steam_database.db"
 
     def add_new_game(self, game_id: str, game_name: str, price: str, link: str):
+        """Write a new tracked game to the database"""
         db = sqlite3.connect(self.db_path)
         cursor = db.cursor()
-        cursor.execute(f"""
-        INSERT INTO Games VALUES
-        ({game_id}, {game_name}, {price}, {link})
-        """)
+        try:
+            cursor.execute(f"""
+            INSERT INTO Games VALUES
+            (?, ?, ?, ?)
+            """, (game_id, game_name, price, link))
+            db.commit()
+        except sqlite3.InternalError:
+            pass
         db.close()
 
     def delete_game(self):
