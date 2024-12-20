429008fa2d005957527cb07a965b60b5886e9c1d
Pavel <asinskijp188@gmail.com>
2024-12-20 11:26:48 +0300

    Added DatabaseGetter class to retrieve information from the database. Getting started with the DatabaseController class

--- steam_database/db_controller.py
+++ steam_database/db_controller.py
@@ -3,10 +3,16 @@ import sqlite3
 
 class DatabaseController:
     def __init__(self):
-        pass
+        self.db_path = "steam_database.db"
 
-    def add_new_game(self):
-        pass
+    def add_new_game(self, game_id: str, game_name: str, price: str, link: str):
+        db = sqlite3.connect(self.db_path)
+        cursor = db.cursor()
+        cursor.execute(f"""
+        INSERT INTO Games VALUES
+        ({game_id}, {game_name}, {price}, {link})
+        """)
+        db.close()
 
     def delete_game(self):
         pass
--- steam_database/db_getter.py
+++ steam_database/db_getter.py
@@ -3,10 +3,24 @@ import sqlite3
 
 class DatabaseGetter:
     def __init__(self):
-        pass
+        self.db_path = "steam_database.db"
 
     def get_discount_games(self):
-        pass
+        db = sqlite3.connect(self.db_path)
+        cursor = db.cursor()
+        cursor.execute("""
+                SELECT * FROM Discounts
+                """)
+        results = cursor.fetchall()
+        db.close()
+        return results
 
     def get_all_games_list(self):
-        pass
+        db = sqlite3.connect(self.db_path)
+        cursor = db.cursor()
+        cursor.execute("""
+        SELECT * FROM Games
+        """)
+        results = cursor.fetchall()
+        db.close()
+        return results
